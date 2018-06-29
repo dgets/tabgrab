256c230bfdb91d3d019b6b5d9ed41b8bcab94fa2
Damon Getsman <damo.gets@gmail.com>
2018-06-29 04:41:27 -0500

    Moving from 'Hello World' into something where I can work on the actual objectives of this extension.

--- background.js
+++ background.js
@@ -1,6 +1,16 @@
-chrome.runtime.onInstalled.addListener(function() {
+/*chrome.runtime.onInstalled.addListener(function() {
+  //so yeah, I don't think that an ANONYMOUS function is the place to
+  //be handling display code
   chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("God fuggin\' ouah.");
+    console.log("God fuggin' ouah.");
   });
-});
+
+  
+});*/
+
+/*chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is green.");
+  });
+});*/
 
