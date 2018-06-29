2555ff017030300dc5d8bb71a45b28f3de1d49c5
Damon Getsman <damo.gets@gmail.com>
2018-06-29 11:02:38 -0500

    Working on #1, table generation.

--- background.js
+++ background.js
@@ -4,13 +4,27 @@
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log("God fuggin' ouah.");
   });
+*/
 
-  
-});*/
+function populateTable(table, rows, cells, content) {
+    var is_func = (typeof content === 'function');
+    if (!table) table = document.createElement('table');
+    for (var i = 0; i < rows; ++i) {
+        var row = document.createElement('tr');
+        for (var j = 0; j < cells; ++j) {
+            row.appendChild(document.createElement('td'));
+            var text = !is_func ? (content + '') : content(table, i, j);
+            row.cells[j].appendChild(document.createTextNode(text));
+        }
+        table.appendChild(row);
+    }
+    return table;
+}
 
-/*chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
-});*/
+//should be used as such:
+/*document.getElementById('tablearea')
+        .appendChild(populateTable(null, 3, 2, function(t, r, c) {
+                        return ' row: ' + r + ', cell: ' + c;
+                     })
+        );*/
 
